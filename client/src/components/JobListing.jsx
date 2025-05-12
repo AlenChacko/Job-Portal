d7a8fa563f735127370d4c47cd91526020c1671f
Alen Chacko <the.alenchacko@gmail.com>
2025-05-13 00:20:00 +0530

    filters set static

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -11,7 +11,7 @@ const JobListing = () => {
   return (
     <div className="container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8">
       {/* Side bar */}
-      <div className="w-full lg:w-1/4 bg-white px-4">
+      <div className="w-full lg:w-1/4 bg-white px-4 lg:sticky lg:top-8 lg:h-[calc(100vh-4rem)] overflow-y-auto">
         {/* Search bar from hero component */}
         {isSearched &&
           (searchFilter.title !== "" || searchFilter.location !== "") && (
