8601633729c1e2b8e12d15e9f56f1f2cd92751ef
Alen Chacko <the.alenchacko@gmail.com>
2025-05-13 12:22:20 +0530

    Added footer component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import JobListing from "../components/JobListing";
 import AppDownload from "../components/AppDownload";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -10,7 +11,8 @@ const Home = () => {
       <Navbar />
       <Hero />
       <JobListing />
-      <AppDownload/>
+      <AppDownload />
+      <Footer />
     </div>
   );
 };
