3adc6c853ffdcebc249aae9870613bcbab79e4a5
Alen Chacko <the.alenchacko@gmail.com>
2025-05-19 11:11:31 +0530

    database connected

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 // db connection
 
 const connectDB = async () => {
-  mongoose.connection.on("Connected", () => {
+  mongoose.connection.on("connected", () => {
     console.log("Database connected");
   });
-  await mongoose.connect(`${process.env.MONGO_URI}/`);
+  await mongoose.connect(`${process.env.MONGO_URI}/InsideJobs`);
 };
 
 export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,22 +1,25 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/db.js";
 
 // Initialize express
 const app = express();
 
+// Connect to database
+await connectDB();
 
 // Middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Routes
-app.get('/',(req,res)=>{
-res.send("API WOrking")
-})
+app.get("/", (req, res) => {
+  res.send("API WOrking");
+});
 
 // PORT
-const PORT = process.env.PORT || 5000
-app.listen(PORT,()=>{
-    console.log('server running')
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
